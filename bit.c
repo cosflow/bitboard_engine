ecd199559f51ffd77c8aaddd34eb64390846d0b6
cosflow <andrescosflo@usal.es>
2024-11-28 13:42:02 +0100

    2

--- bit.c
+++ bit.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 #define U64 unsigned long long
-//casillas en el tablero
+//squares en el tablero
 enum{
     a8, b8, c8, d8, e8, f8, g8, h8,
     a7, b7, c7, d7, e7, f7, g7, h7,
@@ -46,6 +46,8 @@ void print_bitboard(U64 bb)
 
     printf("Bitboard: %llud \n", bb);
 }
+//[side][attacks]
+U64 pawn_attacks[2][64];
 
 int main(){
 
